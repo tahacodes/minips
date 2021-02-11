367335de123535a28755d2ede5ef3e1047c13b44
Taha Farahani <iotahacodes@gmail.com>
2021-02-11 15:38:25 +0330

    adding two other columns

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
-ls /proc | grep [0-9]
+pids=$(ls /proc | grep [0-9])
+
+for PID in $pids; do
+    if [ -z $(readlink -f /proc/${PID}/exe) ]
+    then
+        echo -e "\n${PID}\t\t no exec"
+    else
+        echo -e "\n${PID}\t\t $(readlink -f /proc/${PID}/exe)"
+    fi
+    echo "opened files:"
+    ls -l /proc/${PID}/fd
+done
