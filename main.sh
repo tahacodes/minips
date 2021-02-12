80755ff77512b9324766427a0a36030a74689320
Taha Farahani <iotahacodes@gmail.com>
2021-02-12 05:11:56 +0330

    complete

--- main.sh
+++ main.sh
@@ -3,10 +3,21 @@ pids=$(ls /proc | grep [0-9])
 for PID in $pids; do
     if [ -z $(readlink -f /proc/${PID}/exe) ]
     then
-        echo -e "\n${PID}\t\t no exec"
+        continue
     else
-        echo -e "\n${PID}\t\t $(readlink -f /proc/${PID}/exe)"
+        echo -e "${PID}\t exec: $(readlink -f /proc/${PID}/exe)"
     fi
-    echo "opened files:"
-    ls -l /proc/${PID}/fd
+    echo -e "\topened files:"
+    fd_FILES=/proc/${PID}/fd/*
+    for fd_file in $fd_FILES
+    do
+        # if [[ "$fd_file" = "/proc/${PID}/fd/0" || 
+        #       "$fd_file" = "/proc/${PID}/fd/1" ||
+        #       "$fd_file" = "/proc/${PID}/fd/2" ]]
+        # then
+        #     continue
+        # fi
+        echo -e "\t\t $(readlink -f $fd_file)"
+    done
+    echo -e "\n"
 done
