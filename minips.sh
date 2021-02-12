c4de90036a61c80d5c8ab884e8d1ddd1f5e4d415
Taha Farahani <iotahacodes@gmail.com>
2021-02-12 15:42:59 +0330

    some new comments

--- minips.sh
+++ minips.sh
@@ -3,7 +3,8 @@ pids=$(ls /proc | grep [0-9])
 for PID in $pids; do
     if [ -z $(readlink -f /proc/${PID}/exe) ]
     then
-        continue
+        echo -e "${PID}\t exec: no exec path"
+        # continue
     else
         echo -e "${PID}\t exec: $(readlink -f /proc/${PID}/exe)"
     fi
@@ -11,12 +12,12 @@ for PID in $pids; do
     fd_FILES=/proc/${PID}/fd/*
     for fd_file in $fd_FILES
     do
-        # if [[ "$fd_file" = "/proc/${PID}/fd/0" || 
-        #       "$fd_file" = "/proc/${PID}/fd/1" ||
-        #       "$fd_file" = "/proc/${PID}/fd/2" ]]
-        # then
-        #     continue
-        # fi
+        if [[ "$fd_file" = "/proc/${PID}/fd/0" || 
+              "$fd_file" = "/proc/${PID}/fd/1" ||
+              "$fd_file" = "/proc/${PID}/fd/2" ]]
+        then
+            continue
+        fi
         echo -e "\t\t $(readlink -f $fd_file)"
     done
     echo -e "\n"
